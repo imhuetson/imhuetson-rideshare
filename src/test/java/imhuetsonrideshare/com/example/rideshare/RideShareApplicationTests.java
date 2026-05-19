288cfc5007638b10dbad35da5a83ba3695c1d78c
imhuetson <imhuetson@mail.mccneb.edu>
2026-05-19 07:46:18 -0500

    Set up application

---
+++ src/test/java/imhuetsonrideshare/com/example/rideshare/RideShareApplicationTests.java
@@ -0,0 +1,13 @@
+package imhuetsonrideshare.com.example.rideshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideShareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
